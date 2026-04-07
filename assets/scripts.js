528a18db741a0287cda68a0bc53ed6e6b30b37ed
Vicki Crosson <vicki.crosson@gmail.com>
2026-04-07 07:08:02 -0400

    Update get-involved

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,37 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 
+  function applyExternalLinkAttrs() {
+    $('a[href]').each(function () {
+      var $link = $(this)
+      var href = ($link.attr('href') || '').trim()
+
+      if (!href || href[0] === '#' || href.indexOf('mailto:') === 0 || href.indexOf('tel:') === 0 || href.indexOf('javascript:') === 0) {
+        return
+      }
+
+      var url
+      try {
+        url = new URL(href, window.location.origin)
+      } catch (e) {
+        return
+      }
+
+      var isHttp = url.protocol === 'http:' || url.protocol === 'https:'
+      var isExternal = isHttp && url.origin !== window.location.origin
+      if (!isExternal) return
+
+      $link.attr('target', '_blank')
+
+      var relValue = ($link.attr('rel') || '').toLowerCase().trim()
+      var relParts = relValue ? relValue.split(/\s+/) : []
+      if (relParts.indexOf('noopener') === -1) {
+        relParts.push('noopener')
+      }
+      $link.attr('rel', relParts.join(' ').trim())
+    })
+  }
+
   function updateHomeIcon() {
     var $icon = $('.js-home-icon')
     if (!$icon.length) return
@@ -17,6 +48,7 @@ $(function () {
     }
   }
 
+  applyExternalLinkAttrs()
   updateHomeIcon()
   $(window).on('scroll resize', updateHomeIcon)
 })
